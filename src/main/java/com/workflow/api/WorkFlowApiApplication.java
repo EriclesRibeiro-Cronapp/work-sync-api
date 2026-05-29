b2bfbfd9ba611763821191b18f97a23074e1e040
EriclesRibeiro-Cronapp <ericles.ribeiro@cronapp.io>
2026-05-29 08:43:26 -0300

    first commit

---
+++ src/main/java/com/workflow/api/WorkFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFlowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workflow/api/WorkFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
